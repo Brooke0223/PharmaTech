5d0ce4a3572e2857051b601c883d8591bfdaeca3
Brooke Ryan <brooke0223@yahoo.com>
2022-11-10 06:07:04 -0500

    finished implementating PUT functionality

--- src/Pages/ViewPatient.js
+++ src/Pages/ViewPatient.js
@@ -7,18 +7,16 @@ function ViewPatient() {
   const [patients, setPatients] = useState('');
 
 
-  //fetch Patient Data upon page mount
+  //fetch Patient Data upon page mount, and refresh if patients are deleted
   useEffect(() => {
     fetch('http://flip3.engr.oregonstate.edu:44265/ViewPatient')
     .then(res => res.json())
     .then(data => setPatients(data))
-    // console.log(patients)
   }, [patients])
 
 
   //OnClick handler to modify a patient
   const modifyHandler = (patientID) => {
-    // console.log("MODIFY THIS PATIENT"+patientID)
     navigate("/PharmaTech/editPatient/"+patientID)
   }
 
@@ -30,7 +28,7 @@ function ViewPatient() {
       fetch('http://flip3.engr.oregonstate.edu:44265/DeletePatient/' + patientID, {
         method: 'DELETE',
       })
-      .then(res => res.text()) // or res.json()
+      .then(res => res.text())
       .then(res => console.log(res))
 
 
@@ -42,64 +40,62 @@ function ViewPatient() {
   }
 
   return (
-    
+
     <div className="container">
-    <ul className="nav nav-tabs">
-        <li className="nav-link" onClick={event => navigate("/PharmaTech/ViewPatient")}>
-            View Patients
-        </li>
+      <ul className="nav nav-tabs">
+          <li className="nav-link" onClick={event => navigate("/PharmaTech/ViewPatient")}>
+              View Patients
+          </li>
 
-        <li className="nav-link" onClick={event => navigate("/PharmaTech/searchPatient")}>
-            Search Patients
-        </li>
+          <li className="nav-link" onClick={event => navigate("/PharmaTech/searchPatient")}>
+              Search Patients
+          </li>
 
-        <li className="nav-link" onClick={event => navigate("/PharmaTech/addPatient")}>
-            Add Patient
-        </li>
-    </ul>
+          <li className="nav-link" onClick={event => navigate("/PharmaTech/addPatient")}>
+              Add Patient
+          </li>
+      </ul>
 
       <h1>View Patients</h1>
       <br></br>
-    <table className="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Patient ID</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Middle Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">DOB</th>
-      <th scope="col">Sex</th>
-      <th scope="col">Race</th>
-      <th scope="col">Ethnicity</th>
-      <th scope="col">Active Status</th>
-      <th scope="col">Modify</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Patient ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Middle Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">DOB</th>
+            <th scope="col">Sex</th>
+            <th scope="col">Race</th>
+            <th scope="col">Ethnicity</th>
+            <th scope="col">Active Status</th>
+            <th scope="col">Modify</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
 
-  {Array.isArray(patients) && patients.map((patient, index) => {
-        return (
-          <tbody>
-              <tr key={index}>
-                <td>{patient.PatientID}</td>
-                <td>{patient.FirstName}</td>
-                <td>{patient.MiddleName}</td>
-                <td>{patient.LastName}</td>
-                <td>{patient.DOB.slice(0, 10)}</td>
-                <td>{patient.Sex}</td>
-                <td>{patient.Race}</td>
-                <td>{patient.Ethnicity}</td>
-                <td>{patient.ActiveStatus}</td>
-                <td className="modify" onClick={() => modifyHandler(patient.PatientID)}>⨁</td>
-                <td className="delete" onClick={() => deleteHandler(patient.PatientID)}>⨂</td>
-              </tr>
-          </tbody>
-        );
-      })}
-  
-</table>
-</div>
-
+        {Array.isArray(patients) && patients.map((patient, index) => {
+              return (
+                <tbody>
+                    <tr key={patient.PatientID}>
+                      <td>{patient.PatientID}</td>
+                      <td>{patient.FirstName}</td>
+                      <td>{patient.MiddleName}</td>
+                      <td>{patient.LastName}</td>
+                      <td>{patient.DOB.slice(0, 10)}</td>
+                      <td>{patient.Sex}</td>
+                      <td>{patient.Race}</td>
+                      <td>{patient.Ethnicity}</td>
+                      <td>{patient.ActiveStatus}</td>
+                      <td className="modify" onClick={() => modifyHandler(patient.PatientID)}>⨁</td>
+                      <td className="delete" onClick={() => deleteHandler(patient.PatientID)}>⨂</td>
+                    </tr>
+                </tbody>
+              );
+            })}
+      </table>
+  </div>
   )
 }
 
