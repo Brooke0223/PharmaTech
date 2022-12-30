be81be86bffa01d259c2e3d9bc51f06b186f85d9
Brooke Ryan <brooke0223@yahoo.com>
2022-12-30 17:15:52 -0500

    updated config

--- src/endpoint-config.js
+++
@@ -1,3 +0,0 @@
-export const ENDPOINT = 'https://odd-ruby-swallow-garb.cyclic.app/'
-// export const ENDPOINT = 'http://localhost:44265'
-// export const ENDPOINT = 'http://flip1.engr.oregonstate.edu:44265'
