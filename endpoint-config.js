444e8e5e0750e99479588761b57d38efbec94689
Brooke Ryan <brooke0223@yahoo.com>
2022-12-30 16:25:27 -0500

    updated ENV

--- endpoint-config.js
+++
@@ -1,3 +0,0 @@
-export const ENDPOINT = 'https://pharmatech-backend.onrender.com'
-// export const ENDPOINT = 'http://localhost:44265'
-// export const ENDPOINT = 'http://flip1.engr.oregonstate.edu:44265'
